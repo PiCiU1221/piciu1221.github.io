980a4d2522b15710ff1628786bd9d87408d4fbd7
PiCiU1221 <96005164+PiCiU1221@users.noreply.github.com>
2023-10-05 23:42:59 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://firesignal.onrender.com/:path*",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
